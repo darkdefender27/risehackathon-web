6e5daa20b5604cdc2b8cc79597d661ed7e914472
Anup Sakhare <acsakhare@gmail.com>
2016-09-18 01:30:25 +0530

    First Commit , Basic Rest Server with Security

---
+++ source/server/aggregator-server/src/main/java/com/supernova/AggregatorServerApplication.java
@@ -0,0 +1,13 @@
+package com.supernova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggregatorServerApplication.class, args);
+    }
+
+}
