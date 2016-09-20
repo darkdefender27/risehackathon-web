82eab59d5110c1573d1973fc6dcbb7893dd1a31b
Shubham Utwal <shubhamutwal@gmail.com>
2016-09-20 21:53:24 +0530

    Added standalone ng2 quickstart webapp

--- quickstart/systemjs.config.js
+++ quickstart/systemjs.config.js
@@ -10,6 +10,9 @@
     },
     // map tells the System loader where to look for things
     map: {
+      // our app is within the app folder
+      app: 'app',
+
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
@@ -21,11 +24,20 @@
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
 
       // other libraries
-      'rxjs':                       'npm:rxjs'
+      'rxjs':                       'npm:rxjs',
+      'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
+      app: {
+        main: './main.js',
+        defaultExtension: 'js'
+      },
       rxjs: {
+        defaultExtension: 'js'
+      },
+      'angular2-in-memory-web-api': {
+        main: './index.js',
         defaultExtension: 'js'
       }
     }
