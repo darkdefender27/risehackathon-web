5ab6d4cd4e1d8c7617ef8d59f41e7cde1b523f82
Anup Sakhare <acsakhare@gmail.com>
2016-09-22 00:01:24 +0530

    Updated  failure with Security

--- source/server/aggregator-server/src/main/java/com/supernova/security/RESTAuthenticationFailureHandler.java
+++ source/server/aggregator-server/src/main/java/com/supernova/security/RESTAuthenticationFailureHandler.java
@@ -2,14 +2,13 @@ package com.supernova.security;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
+
 public class RESTAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Override
